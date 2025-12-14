b9eb32f11e4e65f8226ef22c19aca62177f4614a
gwhockett <gwhockett@gmail.com>
2025-12-14 16:08:04 -0500

    Launch School File-based CMS project progress

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "sinatra-contrib"
+gem "erubi"
+gem "webrick"
+
+gem "rack-test"
+gem "minitest"
+
+gem "redcarpet"
+
+ruby "3.2.2"
---
+++ test/cms_test.rb
@@ -0,0 +1,59 @@
+ENV["RACK_ENV"] = "test"
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.md"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+
+  def test_viewing_text_document
+    get "/history.txt"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "Ruby 0.95 released"
+
+    get "/changes.txt"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "changes"
+  end
+
+  def test_viewing_markdown_document
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h2>Ruby is...</h2>"
+  end
+
+  def test_document_not_found
+    get "/nofile.ext" # Attempt to access a nonexistent file
+
+    assert_equal 302, last_response.status # Assert that the user was redirected
+
+    get last_response["Location"] # Request the page that the user was redirected to
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "nofile.ext does not exist"
+
+    get "/" # Reload the page
+    refute_includes last_response.body, "nofile.ext does not exist" # Assert that our message has been removed
+  end
+end
